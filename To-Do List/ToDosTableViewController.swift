bf5496bc878061c579eff0f1f6259546cbc8c965
Daniel <daniel@daniels-macbook.local>
2018-06-19 16:17:17 +0800

    Reuse identifier

--- To-Do List/ToDosTableViewController.swift
+++ To-Do List/ToDosTableViewController.swift
@@ -39,15 +39,13 @@ class ToDosTableViewController: UITableViewController {
         return toDos.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        let cell = tableView.dequeueReusableCell(withIdentifier: "toDoCell", for: indexPath)
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
