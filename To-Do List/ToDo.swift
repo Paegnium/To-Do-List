0137d92da5ca7a4f7de5f3e26346d6998f7d0490
Daniel <daniel@daniels-macbook.local>
2018-06-19 16:09:01 +0800

    Placeholder array, data model class

---
+++ To-Do List/ToDo.swift
@@ -0,0 +1,19 @@
+//
+//  ToDo.swift
+//  To-Do List
+//
+//  Created by Daniel on 19/6/18.
+//  Copyright © 2018 Edgefield. All rights reserved.
+//
+
+import Foundation
+
+class ToDo {
+    var title: String
+    var date: String
+    
+    init(title: String, date: String) {
+        self.title = title
+        self.date = date
+    }
+}
